2d71f65cd6cd72bef96ae42157d3c7148d018708
steve <stevepeckengineer@gmail.com>
2026-08-09 19:12:38 -0400

    wallet withdraw tests for decreasing balance with and without sufficient funds. tests red

--- pointers-errors/wallet_test.go
+++ pointers-errors/wallet_test.go
@@ -16,4 +16,24 @@ func TestWallet(t *testing.T) {
 			t.Errorf("%#v got %s want %s", wallet, got, want)
 		}
 	})
+
+	t.Run("Withdraw decreases balance", func(t *testing.T) {
+		wallet := Wallet{10}
+		wallet.Withdraw(5)
+		got := wallet.Balance()
+		want := Bitcoin(5)
+		if got != want {
+			t.Errorf("%#v got %s want %s", wallet, got, want)
+		}
+	})
+
+	t.Run("Insufficient funds, Withdraw does nothing", func(t *testing.T) {
+		wallet := Wallet{3}
+		wallet.Withdraw(5)
+		got := wallet.Balance()
+		want := Bitcoin(4)
+		if got != want {
+			t.Errorf("%#v got %s want %s", wallet, got, want)
+		}
+	})
 }
