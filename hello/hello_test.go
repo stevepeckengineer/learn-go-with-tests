dfaa99e35a04b2867ce16e9367f045259b3db9d7
steve <stevepeckengineer@gmail.com>
2026-08-08 23:16:58 -0400

    refactored tests to be DRY, passing

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -7,17 +7,20 @@ func TestHello(t *testing.T) {
 	t.Run("Saying hello with name", func(t *testing.T){
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q \n want %q", got, want)
-		}
+		assertEqual(t, got, want)
 	})
+
 	t.Run("Saying hello, world if no name", func(t *testing.T){
 		got := Hello("")
 		want := "Hello, world!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertEqual(t, got, want)
 	})
+}
+
+func assertEqual(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
