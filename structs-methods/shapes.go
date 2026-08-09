549e7cf4f630d6649ecbb5b0c2797ffbc1d1b2fe
steve <stevepeckengineer@gmail.com>
2026-08-09 15:11:12 -0400

    add Rectangle, Area and Perimeter, tests passing

---
+++ structs-methods/shapes.go
@@ -0,0 +1,14 @@
+package shapes
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func Area(r Rectangle) float64 {
+	return r.Width * r.Height
+}
---
+++ structs-methods/shapes_test.go
@@ -0,0 +1,25 @@
+package shapes
+
+import (
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{10.0, 10.0}
+	got := Perimeter(rect)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	rect := Rectangle{10.0, 10.0}
+	got := Area(rect)
+	want := 100.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
