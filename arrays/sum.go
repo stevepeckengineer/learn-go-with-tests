ac0a871a89c7615f44d01be17037519e0bf50cf9
steve <stevepeckengineer@gmail.com>
2026-08-09 11:28:21 -0400

    sums test red

---
+++ arrays/sum.go
@@ -0,0 +1,5 @@
+package sum
+
+func Sum(numbers [5]int) int {
+	return 0
+}
---
+++ arrays/sum_test.go
@@ -0,0 +1,20 @@
+package sum
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("Add an array of nums", func(t *testing.T){
+		nums := [5]int{1,2,3,4,5}
+		got := Sum(nums)
+		want := 15
+		assertEqual(t, got, want, nums[:])
+	})
+}
+
+
+func assertEqual(t testing.TB, got, want int, input []int){
+	t.Helper()
+	if got != want {
+		t.Errorf("got: %d, wanted: %d, input: %v", got, want, input)
+	}
+} 
