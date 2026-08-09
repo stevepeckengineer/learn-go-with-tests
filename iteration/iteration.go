932eab98576107f4fe8845895195df3677780461
steve <stevepeckengineer@gmail.com>
2026-08-09 01:05:22 -0400

    added ability to set # of repeats

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,19 @@
 package iteration
 
-func Repeat(character string) (repeated string) {
-	for i := 0; i < 5; i++ {
-		repeated += character
+import "strings"
+
+const defaultCount = 5
+
+func Repeat(character string, count int) string {
+	var repeated strings.Builder
+
+	if count == 0 {
+		count = defaultCount
 	}
-	
-	return
+
+	for i := 0; i < count; i++ {
+		repeated.WriteString(character)
+	}
+
+	return repeated.String()
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,8 +3,28 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("Default to 5 repeats", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := "aaaaa"
+		assertRepeat(t, repeated, expected)
+	})
+
+	t.Run("Accept a custom number of repeats", func(t *testing.T) {
+		repeated := Repeat("z", 7)
+		expected := "zzzzzzz"
+		assertRepeat(t, repeated, expected)
+	})
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 0)
+	}
+}
+
+func assertRepeat(t testing.TB, repeated, expected string) {
+	t.Helper()
 
 	if repeated != expected {
 		t.Errorf("got %q, expected %q", repeated, expected)
