c6901c7983717c9f3124f47b2f9dbe5025cced87
steve <stevepeckengineer@gmail.com>
2026-08-08 22:29:06 -0400

    initial

---
+++ hello/hello-test.go
@@ -0,0 +1,13 @@
+// Tests for hello world
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello('Chris')
+	want:= "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q \n want %q", got, want)
+	}
+}
