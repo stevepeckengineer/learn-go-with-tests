9b0ee4599660b44a9ac138d39a38cfd500852107
steve <stevepeckengineer@gmail.com>
2026-08-09 12:13:22 -0400

    cleanup redundant test, coverage still 100%

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,13 +3,6 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("Add an array of 5 nums", func(t *testing.T) {
-		nums := [5]int{1, 2, 3, 4, 5}
-		got := Sum(nums[:])
-		want := 15
-		assertEqual(t, got, want, nums[:])
-	})
-
 	t.Run("Add array of any size", func(t *testing.T) {
 		nums := []int{1, 4, 75}
 		got := Sum(nums)
